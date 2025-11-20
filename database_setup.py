76ed8a0c5aa0a840bde5c2aac308381d86d47ec5
Emma <emarion@umich.edu>
2025-11-20 13:31:53 -0500

    Add table 2 and table 3

--- database_setup.py
+++ database_setup.py
@@ -5,15 +5,36 @@ def database_setup():
     conn = sqlite3.connect("starwars.db")
     cursor = conn.cursor()
 
-    table1 = """
+    # Table 1: character table
+    table_1 = """
         CREATE TABLE IF NOT EXISTS characters (
             id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL
+            name TEXT NOT NULL,
+            height INTEGER,
+            homeworld_id INTEGER
         )
-    
     """
 
-    cursor.execute(table1)
+    # Table 2: planet table
+    table_2 = """
+        CREATE TABLE IF NOT EXISTS planets (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            population INTEGER
+        )
+    """
+
+    # Table 5: Media types
+    table_5 = """
+        CREATE TABLE IF NOT EXISTS media_types (
+            id INTEGER PRIMARY KEY,
+            media_type TEXT
+        )
+    """
+
+    cursor.execute(table_1)
+    cursor.execute(table_2)
+    cursor.execute(table_5)
 
     conn.commit()  # save the changes
     conn.close()  # close the connection
