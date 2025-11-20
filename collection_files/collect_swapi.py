5bc595fc3e6f76d235b6c17a3b217801375936c4
Emma <emarion@umich.edu>
2025-11-20 13:31:33 -0500

    remane get_characters to get_data
    add ability to get planet and character data

--- collection_files/collect_swapi.py
+++ collection_files/collect_swapi.py
@@ -2,29 +2,42 @@ import requests
 import sqlite3
 import os
 
+# NOTE:
+# There are 82 people and 60 planets in swapi, which satisfies the
+# "Access and store at least 100 rows in your database from each API/website" requirement
 
-def get_character(character_id=1):
+# People and planets each get their own seperate tables in the database
+# The character table contains a foriegn key from the planet table
+
+
+def get_data(type="character", request_id=1):
     """
-    creates an API request to the swapi
+    creates an API request to the swapi and retreieves information for a SINGLE entry
     ARGUMENTS:
-        character_id: the id of the character
+        type (string): specifies the type of request, either "character" or "planet"
+        request_id (int): the id of the character
     """
 
-    base_url = "http://swapi.dev/api/people/"
+    # Set the base url for the request depending on the type argument
+    base_url = (
+        "http://swapi.dev/api/people/"
+        if type.lower() == "character"
+        else "http://swapi.dev/api/planet/"
+    )
 
-    # convert the character id to a string to append to url
-    character_id_str = str(character_id)
+    # Convert the request id to a string to append to url
+    character_id_str = str(request_id)
 
     # Make the API request
     try:
         request_url = base_url + character_id_str
         response = requests.get(request_url)
-        character_data = response.json()
+        data = response.json()
     except requests.exceptions.RequestException as e:
         print(f"Error fetching API data: {e}")
         exit()
 
-    # Connect to SQLite database
+    # TODO Connect to SQLite database
     pass
 
 
