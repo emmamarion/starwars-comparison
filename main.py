031ed5c0c6ae4c5915109b6aa628b82d4bef0f41
Emma <emarion@umich.edu>
2025-11-20 10:47:15 -0500

    added import statements for other python files

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import database_setup
+import calculations
+import visualizations
+from collection_files import collect_lego
+from collection_files import collect_swapi
+from collection_files import collect_wookiepedia
+
+
 def main():
     print("hello!")
 
