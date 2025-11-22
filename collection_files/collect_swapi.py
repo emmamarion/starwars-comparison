0972bffd43edfa587fcf450c8fe8396cd0deee12
Emma <emarion@umich.edu>
2025-11-22 13:19:58 -0500

    refactor get_data and update_vehicle_table functions for vehicle data handling

--- collection_files/collect_swapi.py
+++ collection_files/collect_swapi.py
@@ -9,35 +9,35 @@ import os
 # People and planets each get their own seperate tables in the database, which satisfies the
 # "For at least one API you must have two tables" requirement.
 
-# The character table contains a foriegn key from the planet table, which satisfies the
+# The vehicle table contains a foriegn key from the planet table, which satisfies the
 # "For at least one API you must have two tables that share an integer key" requirement.
 
 
 # TODO: implement handling for "unknown" values
-def get_data(type="character", request_id=1):
+def get_data(type="vehicle", request_id=1):
     """
     creates an API request to the swapi and retreieves information for a SINGLE entry
 
     ARGUMENTS:
-        type (string): specifies the type of request, either "character" or "planet"
-        request_id (int): the id of the character
+        type (string): specifies the type of request, either "starship" or "vehicle"
+        request_id (int): the id of the vehicle
     RETURNS:
         data: json dictionary of requested data
     """
 
     # Set the base url for the request depending on the type argument
     base_url = (
-        "http://swapi.dev/api/people/"
-        if type.lower() == "character"
-        else "http://swapi.dev/api/planets/"
+        "https://swapi.info/api/starships/"
+        if type.lower() == "starship"
+        else "https://swapi.info/api/vehicles/"
     )
 
     # Convert the request id to a string to append to url
-    character_id_str = str(request_id)
+    vehicle_id_str = str(request_id)
 
     # Make the API request
     try:
-        request_url = base_url + character_id_str
+        request_url = base_url + vehicle_id_str
         response = requests.get(request_url)
         data = response.json()
         return data
@@ -46,12 +46,12 @@ def get_data(type="character", request_id=1):
         exit()
 
 
-def update_character_table(data, database_filename):
+def update_vehicle_table(data, database_filename):
     """
-    Adds character data to the specified database in the "characters" table.
+    Adds vehicle data to the specified database in the "vehicles" table.
 
     ARGUMENTS:
-        data (dict): json data for a character
+        data (dict): json data for a vehicle
         database_filename (string): filename of the target database
     RETURNS:
         None
@@ -61,15 +61,14 @@ def update_character_table(data, database_filename):
     conn = sqlite3.connect(database_filename)
     cursor = conn.cursor()
 
-    # Get and save character information
-    character_name = data.get("name")
-    character_height = int(data.get("height"))
-    character_id = data.get("url", 0)[-2]
-    character_homeworld_id = data.get("homeworld")[-2]
+    # Get and save vehicle information
+    vehicle_name = data.get("name")
+    vehicle_length = int(data.get("length"))
+    vehicle_id = data.get("url", 0)[-2]
 
     cursor.execute(
-        """INSERT INTO characters (id, name, height, homeworld_id) VALUES (?, ?, ?, ?)""",
-        (character_id, character_name, character_height, character_homeworld_id),
+        """INSERT INTO vehicles (id, name, vehicle_length) VALUES (?, ?, ?)""",
+        (vehicle_id, vehicle_name, vehicle_length),
     )
     conn.commit()
     conn.close()
