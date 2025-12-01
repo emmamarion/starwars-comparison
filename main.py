d3bd7a7badeb3641b8268ffc3c634b34ba87c190
Emma <emarion@umich.edu>
2025-12-01 13:33:15 -0500

    remove swapi import

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import database_setup
 import calculations
 import visualizations
 from collection_files import collect_lego
-from collection_files import collect_swapi
 from collection_files import collect_wookiepedia
 
 
