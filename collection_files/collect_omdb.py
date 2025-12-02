1c4fa491749598146a10efe25f6033b7e7612432
Emma <emarion@umich.edu>
2025-12-02 16:47:51 -0500

    add get_api_key function

--- collection_files/collect_omdb.py
+++ collection_files/collect_omdb.py
@@ -1,7 +1,34 @@
 import requests
 import sqlite3
 
-API_KEY = ""
+import re
+
+
+def get_api_key(filename="api_keys.txt"):
+    """
+    Returns the Api Key for the OMDB API.
+
+    Args:
+        filename (str, optinial): filename of textfile where api keys are stored
+
+    Returns:
+        api_key (str):
+    """
+    api_key = None
+    try:
+        with open(filename, "r") as f:
+            text_file_contents = f.read().strip()
+            api_key = re.search(r"omdb: (.*)", text_file_contents)
+            if api_key:
+                api_key = api_key.group(1)
+                print("API KEY LOADED")
+            else:
+                print("WARNING: API key is not loaded.")
+
+    except FileNotFoundError:
+        print(f"ERROR: file: {filename} not found.")
+
+    return api_key
 
 
 def collect_omdb_data():
@@ -12,10 +39,6 @@ def insert_into_database(limit=25):
     pass
 
 
-# For debugging:
-def test():
-    pass
-
-
 if __name__ == "__main__":
-    test()
+    API_KEY = get_api_key()
+    print(API_KEY)
