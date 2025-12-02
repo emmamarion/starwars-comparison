83c6f6bf3af50545913c6cc7297cd37b482dc94a
Emma <emarion@umich.edu>
2025-12-02 16:42:39 -0500

    finished get_api_key function

--- collection_files/collect_lego.py
+++ collection_files/collect_lego.py
@@ -2,15 +2,32 @@ import re
 
 
 def get_api_key(filename="api_keys.txt"):
+    """
+    Returns the Api Key for the Rebrickable API.
+
+    Args:
+        filename (str, optinial): filename of textfile where api keys are stored
+
+    Returns:
+        api_key (str):
+    """
     api_key = None
     try:
         with open(filename, "r") as f:
-            f.read()
-            api_key = f.read().strip()
+            text_file_contents = f.read().strip()
+            api_key = re.search(r"rebrickable: (.*)", text_file_contents)
+            if api_key:
+                api_key = api_key.group(1)
+                print("API KEY LOADED")
+            else:
+                print("WARNING: API key is not loaded.")
+
     except FileNotFoundError:
         print(f"ERROR: file: {filename} not found.")
 
-    if api_key is None:
-        print("WARNING: API key is not loaded.")
-
     return api_key
+
+
+if __name__ == "__main__":
+    API_KEY = get_api_key()
+    print(API_KEY)
