1711156bb378464ea4d5dcf68649d3f40c56b70f
Emma <emarion@umich.edu>
2025-12-11 12:51:05 -0500

    add lego tables

--- database_setup.py
+++ database_setup.py
@@ -14,6 +14,24 @@ def database_setup(filename):
     conn = sqlite3.connect(filename)
     cursor = conn.cursor()
 
+    table_1 = """
+        CREATE TABLE IF NOT EXISTS lego_themes (
+            id   INTEGER PRIMARY KEY,
+            name TEXT UNIQUE
+        )
+    """
+
+    table_2 = """
+        CREATE TABLE IF NOT EXISTS lego_sets (
+            set_num   TEXT PRIMARY KEY,
+            name      TEXT NOT NULL,
+            year      INTEGER,
+            num_parts INTEGER,
+            theme_id  INTEGER,
+            FOREIGN KEY(theme_id) REFERENCES lego_themes(id)
+        )
+    """
+
     # Table 5: Media types
     table_5 = """
         CREATE TABLE IF NOT EXISTS comics (
@@ -23,6 +41,8 @@ def database_setup(filename):
         )
     """
     # Parent tables
+    cursor.execute(table_1)
+    cursor.execute(table_2)
     cursor.execute(table_5)  # Create Comic Table
 
     conn.commit()  # save the changes
